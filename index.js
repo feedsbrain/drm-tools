187e9a83e34fbfa1ef1ec7d09bca4cba69cc94ee
Indra Gunawan <feedsbrain@gmail.com>
2019-06-20 18:09:41 +0800

    Flow Structure

--- index.js
+++ index.js
@@ -6,32 +6,43 @@ const widevine = require('./lib/widevine')
 let command
 
 program
-  .command('widevine')
-  .description('widevine drm tools')
+  .command('widevine [operation]')
+  .description('Widevine DRM Command Line Tools')
   .option('--content-id [id]', 'Content ID')
   .option('--url [url]', 'Target key server URL')
   .option('--provider [provider]', 'Provider (for Widevine)')
   .option('--key [key]', 'Private Key')
   .option('--key-iv [kiv]', 'Private Key IV')
-  .action(function(){
+  .action(function (operation, options) {
     command = 'widevine'
-    if (!program.contentId) {
-      program.help()
+    if (operation === 'key') {
+      if (!options.contentId || !options.url || !options.provider || !options.key || !options.keyIv) {
+        this.help()
+        process.exit(1)
+      }
+      let params = {
+        contentId: options.contentId,
+        widevineUrl: options.url,
+        provider: options.provider,
+        privateKey: options.key,
+        privateKeyIV: options.keyIv
+      }
+
+      widevine.getKeys(params).then((keys) => {
+        console.log(JSON.stringify(keys, null, 2))
+      })
+    }
+    if (!operation) {
+      this.help()
       process.exit(1)
     }
-    let params = {
-      contentId: program.contentId,
-      widevineUrl: program.url,
-      provider: program.provider,
-      privateKey: program.key,
-      privateKeyIV: program.keyIv
-    }
-  
-    console.log(params)
-    widevine.getKeys(params).then((keys) => {
-      console.log(keys)
-    })
   })
+
+program.on('command:*', function () {
+  console.error('Invalid Command: %s\n', program.args.join(' '))
+  program.help()
+  process.exit(1)
+})
 
 program
   .usage('<command> -h for help')
