9145c77ff970a9a656009edecb091367510a560b
Indra Gunawan <feedsbrain@gmail.com>
2019-06-20 13:20:30 +0800

    Widevine content key retrieval

--- index.js
+++ index.js
@@ -4,30 +4,39 @@ const program = require('commander')
 const widevine = require('./lib/widevine')
 
 program
-  .version('0.1.0', '-v, --version')
-  .option('-w, --widevine', 'Generate Widevine Keys')
-  .option('-a, --asset-id [asset-id]', 'Asset ID')
-  .option('-U, --key-server-url [key-server-url]', 'Target Key Server')
-  .option('-P, --provider [provider]', 'Provider')
-  .option('-K, --private-key [private-key]', 'Private Key')
-  .option('-V, --private-key-iv [private-key-iv]', 'Private Key IV')
+  .usage('[options]')
+  .version('0.1.0', '--version')
+  .option('-w, --widevine', 'Get Widevine content keys')
+  .option('-r, --playready', 'Get Playready content keys')
+  .option('-c, --content-id [id]', 'Content ID')
+  .option('-u, --url [url]', 'Target key server URL')
+  .option('-p, --provider [provider]', 'Provider (for Widevine)')
+  .option('-k, --key [key]', 'Private Key')
+  .option('-v, --kiv [kiv]', 'Private Key IV')
   .parse(process.argv)
 
 if (program.widevine) {
-  if (!program.assetId) {
-    console.log('Asset ID is Missing')
+  if (!program.contentId) {
+    console.log('Content ID is Missing')
     process.exit(1)
   }
   let params = {
-    assetId: program.assetId,
-    widevineUrl: program.keyServerUrl,
+    contentId: program.contentId,
+    widevineUrl: program.url,
     provider: program.provider,
-    privateKey: program.privateKey,
-    privateKeyIV: program.privateKeyIv
+    privateKey: program.key,
+    privateKeyIV: program.kiv
   }
 
   console.log(params)
+  widevine.getKeys(params).then((keys) => {
+    console.log(keys)
+  })
+}
 
-  const result = widevine.getKeys(params)
-  console.log(result)
+if (program.playready) {
+}
+
+if (!program.widevine && !program.playready) {
+  program.help()
 }
