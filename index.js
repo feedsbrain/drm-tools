a496c31a8cf3df31d49cc8919c84355f88f06728
Indra Gunawan <feedsbrain@gmail.com>
2019-06-20 16:10:30 +0800

    Checkpoint before rename

--- index.js
+++ index.js
@@ -3,40 +3,46 @@
 const program = require('commander')
 const widevine = require('./lib/widevine')
 
+let command
+
 program
-  .usage('[options]')
-  .version('0.1.0', '--version')
-  .option('-w, --widevine', 'Get Widevine content keys')
-  .option('-r, --playready', 'Get Playready content keys')
-  .option('-c, --content-id [id]', 'Content ID')
-  .option('-u, --url [url]', 'Target key server URL')
-  .option('-p, --provider [provider]', 'Provider (for Widevine)')
-  .option('-k, --key [key]', 'Private Key')
-  .option('-v, --kiv [kiv]', 'Private Key IV')
+  .command('widevine')
+  .description('use -h for list of available options')
+  .option('--content-id [id]', 'Content ID')
+  .option('--url [url]', 'Target key server URL')
+  .option('--provider [provider]', 'Provider (for Widevine)')
+  .option('--key [key]', 'Private Key')
+  .option('--key-iv [kiv]', 'Private Key IV')
+  .action(function(){
+    command = 'widevine'
+    if (!program.contentId) {
+      console.log('Content ID is Missing')
+      process.exit(1)
+    }
+    let params = {
+      contentId: program.contentId,
+      widevineUrl: program.url,
+      provider: program.provider,
+      privateKey: program.key,
+      privateKeyIV: program.keyIv
+    }
+  
+    console.log(params)
+    widevine.getKeys(params).then((keys) => {
+      console.log(keys)
+    })
+  })
+
+program
+  .usage('<command> [options] (use -h or --help for list of available options)')
+  .version('0.1.0', '-v, --version')
+  .arguments('<cmd> [env]')
+  .action(function (cmd, env) {
+    cmdValue = cmd;
+    envValue = env;
+  })
   .parse(process.argv)
 
-if (program.widevine) {
-  if (!program.contentId) {
-    console.log('Content ID is Missing')
-    process.exit(1)
-  }
-  let params = {
-    contentId: program.contentId,
-    widevineUrl: program.url,
-    provider: program.provider,
-    privateKey: program.key,
-    privateKeyIV: program.kiv
-  }
-
-  console.log(params)
-  widevine.getKeys(params).then((keys) => {
-    console.log(keys)
-  })
-}
-
-if (program.playready) {
-}
-
-if (!program.widevine && !program.playready) {
+if (!command) {
   program.help()
 }
