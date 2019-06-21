24fa5f17805c139dde0b6e5b6b8aa937ff201fe2
Indra Gunawan <feedsbrain@gmail.com>
2019-06-21 14:30:49 +0800

    Sub Command Structure

--- index.js
+++ index.js
@@ -3,25 +3,38 @@
 const program = require('commander')
 const widevine = require('./lib/widevine')
 
-let command
+let showHelp = true
 
 program
-  .command('widevine [operation]')
-  .description('Widevine DRM Command Line Tools')
+  .command('widevine [operation]', /^(pssh|key|help)$/i, 'help')
+  .usage('[operation] [options]\n\nOperations:\n - key : Tools to get Widevine content key\n - pssh: Tools to work with Widevine PSSH')
   .option('--content-id [id]', 'Content ID')
   .option('--url [url]', 'Target key server URL')
   .option('--provider [provider]', 'Provider (for Widevine)')
   .option('--key [key]', 'Private Key')
   .option('--key-iv [kiv]', 'Private Key IV')
+  .option('--track [value]', 'DRM Track Type', /^(SD|HD|AUDIO|ALL)$/i, 'ALL')
   .action(function (operation, options) {
-    command = 'widevine'
-    if (operation === 'key') {
+    showHelp = false
+    if (operation && operation.toLowerCase() === 'key') {
       if (!options.contentId || !options.url || !options.provider || !options.key || !options.keyIv) {
         this.help()
         process.exit(1)
       }
+      let tracks = [
+        { type: 'SD' },
+        { type: 'HD' },
+        { type: 'AUDIO' }
+      ]
+      if (options.track && options.track.toUpperCase() !== 'ALL') {
+        // Individual Track
+        tracks = [
+          { type: options.track.toUpperCase() }
+        ]
+      }
       let params = {
         contentId: options.contentId,
+        tracks: tracks,
         widevineUrl: options.url,
         provider: options.provider,
         privateKey: options.key,
@@ -32,7 +45,9 @@ program
         console.log(JSON.stringify(keys, null, 2))
       })
     }
-    if (!operation) {
+    if (operation && operation.toLowerCase() === 'pssh') {
+    }
+    if (!operation || (operation && operation.toLowerCase() === 'help')) {
       this.help()
       process.exit(1)
     }
@@ -45,10 +60,11 @@ program.on('command:*', function () {
 })
 
 program
+  .description('Widevine DRM Command Line Tools')
   .usage('<command> -h for help')
   .version('0.0.1', '-v, --version')
   .parse(process.argv)
 
-if (!command) {
+if (showHelp) {
   program.help()
 }
